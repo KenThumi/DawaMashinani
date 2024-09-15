02316eb68dbabec0bee4c6d01983a76d962d076b
Kenneth Mwangi <kenthumi@gmail.com>
2024-09-15 13:37:21 +0300

    Added BaseLayout

--- login_app/forms.py
+++ login_app/forms.py
@@ -18,8 +18,35 @@ class UserForm(UserCreationForm):
         return username
 
 class UserLoginForm(forms.Form):
-    role = forms.ChoiceField(choices=[(name, name) for name in CustomUser.objects.values_list('role', flat=True).distinct()])
+    # role = forms.ChoiceField(choices=[(name, name) for name in CustomUser.objects.values_list('role', flat=True).distinct()])
+
+    db_roles = [(name, name) for name in CustomUser.objects.values_list('role', flat=True).distinct()]
+
+    sl_role = [  
+                ('','--select--')
+             ]
+    
+    roles =  sl_role+db_roles
+
+    role =  forms.CharField( widget=forms.Select(choices=roles))
     
     username = forms.CharField(max_length=100)
 
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['role'].widget.attrs.update({
+            'class':'form-control',
+            'placeholder':'Role',
+        })
+
+        self.fields['username'].widget.attrs.update({
+            'class':'form-control',
+            'placeholder':'username',
+        })
+
+        self.fields['password'].widget.attrs.update({
+            'class':'form-control',
+        })
